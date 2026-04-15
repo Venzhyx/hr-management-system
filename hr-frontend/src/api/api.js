f31a1534b888fcb7fd0758e2b58b827fd08f34df
Venzhyx <saifulgifarievandwi@gmail.com>
2026-04-15 17:04:06 +0700

    edit api

--- hr-frontend/src/api/api.js
+++ hr-frontend/src/api/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
+    Accept: "application/json",
   },
   maxContentLength: Infinity,
   maxBodyLength: Infinity,
